901ec7844e76522e0dd09aadde64f94f013381ed
John's code hut <compres.sdev@gmail.com>
2020-01-22 22:09:08 +0000

    first new commit
    this is an addititon of the tile and level classes as well as a modification of the typedef locations and the addition of the "str" typdef

---
+++ EPQ_code/headers/level_class.h
@@ -0,0 +1,21 @@
+#ifndef LEVEL_CLASS_H_INCLUDED
+#define LEVEL_CLASS_H_INCLUDED
+
+/*
+Code written by John Edgar:
+*/
+
+class Level
+{
+public:
+    Level(){}
+
+    Level(int floor, int seed)
+    {
+
+    }
+
+private:
+};
+
+#endif // LEVEL_CLASS_H_INCLUDED
--- EPQ_code/headers/main_variables.h
+++ EPQ_code/headers/main_variables.h
@@ -1,8 +1,6 @@
 #ifndef MAIN_VARIABLES_H_INCLUDED
 #define MAIN_VARIABLES_H_INCLUDED
 
-typedef int index;
-
 sf::RenderWindow window(sf::VideoMode(600.f, 600.f), "Pixel Slasher");
 
 #endif // MAIN_VARIABLES_H_INCLUDED
